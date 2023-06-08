f37c8abf9d845b27cd1b8bcb3879ae880aa56925
ivanciniGT <ivan.osuna.ayuste@gmail.com>
2023-06-08 10:19:30 +0200

    cucumber

--- proyectos/cucumber/features/steps/servicio.usuarios.steps.js
+++ proyectos/cucumber/features/steps/servicio.usuarios.steps.js
@@ -2,64 +2,74 @@ const {When, Then, Given} =require("@cucumber/cucumber")
 const chai =require("chai")
 const ServicioUsuarios = require("../../ServicioUsuarios.js")
 
-var datosRespuesta = undefined
+// Fase 1: Montar un mock del backend
 
 Given('un usuario con el id {int}', function (int) {
 });
 
-Given('con {string}: {string}', function (propiedad, valor) {
+Given('con {string}: {string}', function (string, string2) {
 });
-Given('con {string}: {int}', function (propiedad, valor) {
+
+Given('con {string}: {int}', function (string, int) {
 });
 
 Given('el usuario es accesible usando un servicio backend de mentirijillla', function () {
 });
 
-////// DE AQUI PARA ABAJO
 
-When('se invoca la función {string}, con el valor {int} y una función de ' +
-        'callback que captura su respuesta', function (funcion, argumento) {
-            const callback = (dato1, dato2)=>{
-                this.respuesta = [dato1,dato2]
-            }
-            switch(funcion){
-                case "getUsuario":
-                    ServicioUsuarios.getUsuario(argumento, callback)
-                    break
-                case "createUsuario":
-                    //getUsuario(argumento, callback)
-                    break
-                case "deleteUsuario":
-                    //deleteUsuario(argumento, callback)
-                    break
-                }
-        });
+//// Fase 2: Montar las pruebas atacando al backend de mentirijilla desde nuestro servicio de usuarios
 
-Then('la respuesta debe contener {int} argumentos', function (numeroDatosRespuesta) {
-    chai.expect(this.respuesta).to.have.lengthOf(numeroDatosRespuesta)
-});
-
-Then('el argumento {int} debe tener un {string}', function (argumento, tipoDeDatos) {
-    var tipo = "number"
-    switch(tipoDeDatos){
-        case "numero":
-            tipo = "number"
-            break
-        case "objeto json":
-            tipo = "object"
-            break
+When('se invoca la función {string}, con el valor {int} y una función de callback que captura su respuesta', function (nombreFuntion, identificador) {        
+    var funcion;
+    switch(nombreFuntion){
+        case "getUser":
+            funcion = ServicioUsuarios.getUser;
+            break;
+        case "deleteUser":
+            funcion = ServicioUsuarios.deleteUser;
+            break;
+        default:
+            throw new Error("Funcion no implementada");
     }
-    chai.expect(this.respuesta[argumento-1]).to.be.a(tipo)
+    funcion(identificador, (error, user) => {
+        // Capturamos los datos y los guardamos para su posterior uso
+        this.respuesta = [error,user];
+    });
 });
 
-Then('el argumento {int} debe tener el valor {int}', function (argumento, valor) {
-    chai.expect(this.respuesta[argumento-1]).to.equals(valor)
+Then('la respuesta debe contener {int} argumentos', function (numeroArgumentos) {
+    chai.expect(this.respuesta).to.have.lengthOf(numeroArgumentos);
 });
 
-Then('el argumento {int} tiene por {string}: {string}', function (argumento, propiedad, valor) {
-    chai.expect(this.respuesta[argumento-1]).to.have.property(propiedad)
-    chai.expect(this.respuesta[argumento-1][propiedad]).to.equals(valor)
+Then('el argumento {int} debe llegar vacio', function (numeroArgumento) {
+    chai.expect(this.respuesta[numeroArgumento]).to.be.null;
 });
+
+Then('el argumento {int} debe tener un {string}', function (numeroArgumento, nombreTipoDeDato) {
+    var tipoDeDato;
+    switch(nombreTipoDeDato){
+        case "objeto json":
+            tipoDeDato = "object";
+            break;
+    }
+
+    chai.expect(this.respuesta[numeroArgumento]).to.be.a(tipoDeDato);
+});
+
+Then('el argumento {int} tiene por {string}: {string}', function (numeroArgumento, campo, valor) {
+    chai.expect(this.respuesta[numeroArgumento][campo]).to.be.equal(valor);
+});
+
+Then('el argumento {int} tiene por {string}: {int}', function (numeroArgumento, campo, valor) {
+    chai.expect(this.respuesta[numeroArgumento][campo]).to.be.equal(valor);
+});
+
+
+
+
+
+
+
 
 
 Given('un servicio backend de mentirijillla', function () {
@@ -68,12 +78,11 @@ Given('un servicio backend de mentirijillla', function () {
 Given('el servicio no tiene el usuario con id {int}', function (int) {
 });
 
-Then('en la segunda posición no tengo nada', function () {
+Then('el argumento {int} no debe llegar vacio', function (int) {
 });
 
 Given('que no hay servicio de backend operativo', function () {
+
 });
 
-Then('en la primera posición se devuelve el valor {int}??', function (int) {
-});
 
